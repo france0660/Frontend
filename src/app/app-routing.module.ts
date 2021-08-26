21428fc411eed0aee7b60b1661405336136db09e
france060 <pakorn_france98@hotmail.com>
2021-08-26 23:49:12 +0700

    no message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LayoutComponent } from './shared/layout/layout.component';
+
 const routes: Routes = [
   
   {
@@ -9,11 +11,18 @@ const routes: Routes = [
     redirectTo: '/login',
     pathMatch: 'full'
 },
+{
+  path: "",
+  component: LayoutComponent,
+  children: [
+    { path: "home", component: HomeComponent }
+  ],
+},
   
   
   
   { path: "login", component: LoginComponent },
-  { path: "home", component: HomeComponent }
+  
 
 ];
 
